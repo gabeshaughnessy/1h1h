272e784cc5fe06cc0f4a0d562fddbd53e2e60fc6
gabeshaughnessy <gabeshaughnessy@gmail.com>
2012-10-28 16:36:16 -0700

    showcase template

---
+++ page-category.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The single page template.
+ *
+ *
+ *
+ * @package WordPress
+ * @subpackage 1H1H
+ */
+
+get_header(); 
+wp_nav_menu( array('menu' => 'Global Menu' ));
+?>
+
+			<div class="section">
+		<div class="wrapper post-type-wrapper">
+			<?php if ( have_posts() ) : ?>
+
+		
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+									<h2 class="section-title"><?php the_title(); ?></h2>
+									<p class="section-logo">One Hat One Hand</p>
+								<div  class="post-box">
+									<div class="primary">
+											<div class="content" role="main">
+											<?php
+										the_content();
+											?>
+										</div>
+									</div>
+								</div>
+								
+								
+					
+						</div><!-- end case_studies wrapper -->
+					</div><!-- end case_studies section -->
+				<?php endwhile; ?>
+
+				
+
+			<?php else : ?>
+			<?php endif; ?>
+
+
+
+
+<?php get_footer(); ?>
---
+++ page-showcase.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Template Name: Showcase
+ *
+ *
+ *
+ * @package WordPress
+ * @subpackage 1H1H
+ */
+
+get_header(); 
+wp_nav_menu( array('menu' => 'Global Menu' ));
+?>
+
+			<div id="portfolio" class="section">
+					<div id="portfolio-wrapper" class="wrapper">
+					
+					
+					<?php //Portfolio Loop Goes Here
+					hh_portfolio_loop('hh_project', 10);
+					/* +++++ T H E   P O R T F O L I O   L O O P ++++++ */
+					//////////////////////////////////////////////////////
+					function hh_portfolio_loop($hhpost_type, $hhcount){ ?>
+					
+					
+								<?php 
+								$args = array(
+									
+											'post_type' => $hhpost_type,
+											'post_count' => $hhcount
+										
+								);
+								$custom_query = new WP_Query( $args );
+								if ( $custom_query->have_posts() ) : ?>
+					
+								
+					
+									<?php /* Start the Loop */ ?>
+									
+									<?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
+									<div class="portfolio-entry post" data-target="<?php the_permalink(); ?>" id="portfolio_post_<?php echo get_the_ID(); ?>">
+									<?php hh_get_portfolio_backgrounds("full-bg"); ?>
+									<!--<h2 class="portfolio-title"><?php the_title(); ?></h2>-->
+									</div><!-- end post -->
+									<?php endwhile; ?>
+					
+									
+					
+								<?php else : ?>
+					
+									<p> No Items to display </p>
+					
+								<?php endif; 
+								// Reset Post Data
+								wp_reset_postdata();
+								?>
+						
+					<?php }
+					//End Portfolio_Loop()
+					
+					//print out each portfolio post with a large backround image that is absolute positioned and full-screen
+					//it would be similar to the homepage loop on zach coffin's site. 
+					//Then build a thumbnail menu that appears at first below the main menu, then slides up out of view except a tab to reactivate it (and keyboard comman "i")
+					
+					//Use a fluid width carousel plugin that has: thumbnail pagers, slider controls
+					 //using cycle.js
+					
+					//The title and the slider controller get absolute positioned over the portfolio entries in the center of the page.
+					
+					/* zach does it with cycle:
+						 $('.cycle #content').cycle({
+						  fx: 'fade', 
+					   speed: 300, 
+					   timeout: 200000,
+					   containerResize: 0		 });
+					   
+					   Each post has a background image container with width: 100%, height: auto and fixed position, overflow hidden and z-index: -2
+					   then inside that container is an img element with the post image also width 100% and height auto
+					   
+					   */
+					 ?>
+				</div>
+				<div id="portfolio-control">
+					<div id="portfolio-title" > <span class="hh_text">One Hat One Hand</span> <span class="fredericka" >Portfolio</span></div>
+				
+					<div id="slider"></div>
+				</div>
+				
+			</div>
+
+
+
+<?php get_footer(); ?>
